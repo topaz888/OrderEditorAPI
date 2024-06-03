a80f74979865773a5f9587cff39054d8e816513a
Tobias Cai <yuhong_cai@hotmail.com>
2024-06-03 12:11:22 -0400

    change IP

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config({ path: './.env' })
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 const IP = process.env.IP
 
 // Middleware to parse JSON bodies
