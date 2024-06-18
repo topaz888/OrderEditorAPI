dde3f574e1a1b1318a15aea58668f556e1e27168
Tobias Cai <yuhong_cai@hotmail.com>
2024-06-18 16:18:58 -0400

    SSL certification

--- database.js
+++ database.js
@@ -1,7 +1,6 @@
 import pkg from 'pg';
 const { Client } = pkg;
 
-
 function serializeDataBaseData(_response) { 
     const orderData = _response.orderEditCommit
     const appendValue = [
@@ -27,7 +26,8 @@ async function syncToDatabase(_response) {
       password: process.env.DATABASE_PASSWORD,
       port: process.env.DATABASE_PORT,
       ssl: {
-        rejectUnauthorized: false,  // For development, you can set this to false; for production, use proper SSL certificate validation
+        rejectUnauthorized: true,
+        ca: process.env.SSL_CA,
       }
     });
   
@@ -36,7 +36,7 @@ async function syncToDatabase(_response) {
     // await client.query('TRUNCATE googlesheet_database');
     // await client.query('CREATE TABLE googleSheet_database(id serial not null primary key, order_number varchar(255) NOT NULL, order_id varchar(255) NOT NULL, customer_name varchar(255) NOT NULL, customer_email varchar(255), customer_phone varchar(255), total_outstanding varchar(255), created_at varchar(255))');
     const insertQuery = 'INSERT INTO googleSheet_database (order_number, order_id, customer_name, customer_email, customer_phone, total_outstanding, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7)';
-    for (let row of data.slice(1)) {  // Skip header row
+    for (let row of data) {
       await client.query(insertQuery, row);
     }
     await client.end();
